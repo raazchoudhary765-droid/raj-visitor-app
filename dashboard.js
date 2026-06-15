5020f8219474bc670a467710ecebc1ab51eb56f8
raazchoudhary765-droid <raazchoudhary765@gmail.com>
2026-06-16 01:23:33 +0530

    Add initial dashboard.js with demo visitor values

---
+++ dashboard.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Demo Values
+    document.getElementById("totalVisitorsCard").innerText = 0;
+
+    document.getElementById("menVisitors").innerText = 0;
+
+    document.getElementById("womenVisitors").innerText = 0;
+
+    document.getElementById("childrenVisitors").innerText = 0;
+
+    document.getElementById("peakHour").innerText =
+    "No Data";
+
+    document.getElementById("peakDay").innerText =
+    "No Data";
+
+});
